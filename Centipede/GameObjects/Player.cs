593fcc6031d12e397247f091e1fc3e356d55ef3b
Blazehunterx <55129855+Blazehunterx@users.noreply.github.com>
2020-03-24 12:22:08 +0100

    Add project files.

---
+++ Centipede/GameObjects/Player.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Centipede.GameObjects {
+    class Player : SpriteGameObject {
+
+        public Player() : base("spr_player") {
+            Mouse.SetPosition(235, 500);
+            Origin = new Vector2(Width / 2, Height / 2);
+        }
+
+        public override void Update(GameTime gameTime) {
+            base.Update(gameTime);
+            
+        }
+    }
+}
---
+++ Centipede/GameStates/PlayingState.cs
@@ -0,0 +1,15 @@
+﻿using Centipede.GameObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Centipede.GameStates {
+    class PlayingState : GameObjectList{
+        Player thePlayer;
+        public PlayingState()  {
+            this.Add(new SpriteGameObject("spr_background"));
+        }
+    }
+}
